a0d41276ebf51812d36267f041a2922856d1e0b6
Ahmet Hakan Hafif <59022934+colegion@users.noreply.github.com>
2024-11-05 18:23:42 +0300

    Bug fix on player pref and wllet

--- Assets/Scripts/Utilities/Wallet.cs
+++ Assets/Scripts/Utilities/Wallet.cs
@@ -7,7 +7,7 @@ namespace Utilities
     public static class Wallet
     {
         private static Dictionary<Utility.RewardType, int> _playerRewards;
-        public static bool UIShouldUpdate = false;
+        public static bool UIShouldUpdate = true;
         public static void AddReward(Utility.RewardType type)
         {
             if (!_playerRewards.TryAdd(type, 1))
@@ -25,7 +25,7 @@ namespace Utilities
             _playerRewards = new Dictionary<Utility.RewardType, int>();
             for (int i = 0; i < Enum.GetValues(typeof(Utility.RewardType)).Length; i++)
             {
-                var rewardCount = PlayerPrefs.GetInt(i.ToString(), 0);
+                var rewardCount = PlayerPrefs.GetInt(((Utility.RewardType)i).ToString(), 0);
                 if (rewardCount > 0)
                 {
                     _playerRewards.Add((Utility.RewardType)i, rewardCount);
